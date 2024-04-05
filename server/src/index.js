b22a3787680bfb8a907a7412fa90ec9577b08a71
devnshankar <devnshankarofficial@gmail.com>
2024-04-05 18:28:13 +0530

    Added a simple express server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello world");
+import express from 'express'
+
+function main () {
+  try {
+    const app = express();
+
+    app.use(express.json())
+
+    app.listen(8082, () => {
+      console.log("Server is running on port 8082")
+    })
+
+    app.get("/" , (req, res) => {
+      res.send("Hello world")
+    } )
+  } catch (error) {
+    console.log(error)
+  }
+}
